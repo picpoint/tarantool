6351cfc66b9ce538e3d93428df2761af15fa6fb6
rmtar <picpoint@yandex.ru>
2026-03-10 23:46:46 +0300

    work with ping

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from ping3 import ping, verbose_ping
 
-
-print(ping("8.8.8.8"))
+for x in range(1, 255):
+    res = ping(f"192.168.10.{x}")
+    if res:
+        print(f"Host 192.168.10.{x} => ACTIVE")
+    else:
+        print(f"Host 192.168.10.{x} => is dead ...")
