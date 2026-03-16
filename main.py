319f69ea4c941041bbf96fbfd093cf73f6d01ebf
Таран Роман Михайлович <r.taran@avtr-adm2.avtodor.sta>
2026-03-16 17:51:31 +0300

    work with get hostname

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import socket
+# import ldap
+from dns import resolver,reversename
 from ping3 import ping, verbose_ping
 
+# # hostname = socket.gethostbyaddr("192.168.24.94")
+# # print(hostname)
 
+# socket.gethostname()
+# res = socket.getfqdn("192.168.24.90")
+# print(res)
+
+# print(socket.gethostbyaddr("192.168.24.90"))
+hostname = socket.gethostbyaddr('192.168.24.90')
+print(hostname)
+
+
+'''
 def ping_network(part_ip):
     for x in range(1, 255):
         res = ping(f"{part_ip}.{x}")
@@ -10,3 +25,4 @@ def ping_network(part_ip):
             print(f"Host {part_ip}.{x} => is dead ...")
 
 ping_network("192.168.24")
+'''
